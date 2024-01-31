70313fd24e5a5f272d285e6b02a1992d3c07cb9f
gilvinda dsouza <gilvindadsouza@gmail.com>
2024-01-31 10:19:24 +0530

    commit

---
+++ src/practice/Caterpillar_xxx.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Caterpillar_xxx {
+	int moves,cabbages;
+	
+	Caterpillar_xxx(int moves, int cabbages)
+	{
+		this.moves = moves;
+		this.cabbages = cabbages;
+	}
+}
